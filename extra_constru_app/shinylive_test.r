3bbd1c8199b3afc944c60a0b5128017a719b5705
Julia Chifman <42590316+chifman@users.noreply.github.com>
2024-11-21 09:37:49 -0500

    Removed some folders

--- extra_constru_app/shinylive_test.r
+++
@@ -1,4 +0,0 @@
-library("shinylive")
-export("shinylive_test","shinylive_test2")
-
-# cd shinylive_test2; python3 -m http.server
--- extra_constru_app/shinylive_test.sh
+++
@@ -1,3 +0,0 @@
-rm -rf shinylive_test2
-Rscript shinylive_test.r
-cd shinylive_test2; python3 -m http.server
